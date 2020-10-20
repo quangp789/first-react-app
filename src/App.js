47a763505e10e4d1d0212926d9125cbd8db1a088
Quang Phong <quangphong@Quangs-MacBook-Pro.local>
2020-10-19 21:57:37 -0500

    List and conditionals practice

--- src/App.js
+++ src/App.js
@@ -1,43 +1,67 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-	// useState - Always returns an array with 2 elements and ONLY 2. 
-	// We use the first arg to render, and the second to make changes
-	const [personsState, setPersonsState] = useState({
+class App extends Component {
+	state = {
 		persons: [
-			{ name: 'Max', age: 28 },
-			{ name: 'Manu', age: 29 },
-			{ name: 'Billy', age: 25 }
+			{id: 'uniqueID1', name: 'Max', age: 28 },
+			{id: 'uniqueID2', name: 'Manu', age: 29 },
+			{id: 'uniqueID3', name: 'Billy', age: 25 }
 		],
-	});
+		otherState: 'other value',
+		showPersons: false
+	}
 	
 	// React Hook: You can reuse "useState" to effectively change your "persons" state w/o affecting your "other" state. 
-	const [otherState, setOtherState] = useState('Some value');
+	//const [otherState, setOtherState] = useState('Some value');
 	
-	const switchNameHandler = (newName) => {
+	switchNameHandler = (newName) => {
 	// This will not work: this.state.persons[0].name = 'Quang';
-		setPersonsState({
+		this.setState( {
 		  persons: [
 			{ name: newName, age: 28 },
 			{ name: 'Manu', age: 29 },
 			{ name: 'Billy', age: 27 }
 		  ]
 		})
-	};
+	}
 	
-	// We extract the "target" of the event (what we typed) and get the value to change the name 
-	const nameChangedHandler = (event) => {
-		setPersonsState({
-		  persons: [
-			{ name: 'Quang', age: 28 },
-			{ name: event.target.value, age: 29 },
-			{ name: 'Billy', age: 27 }
-		  ]
-		})
-	};
+	nameChangedHandler = (event, id) => {
+		// Obtain the position of the array
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id; 
+		});
+		// Store that position
+		const person = {...this.state.persons[personIndex]};
+		
+		// Extract the "target" of the event (what we typed)
+		person.name = event.target.value;
+		
+		// Update the position
+		const persons  = [...this.state.persons];
+		persons[personIndex] = person;
+		
+		// Set the state
+		this.setState({persons: persons})
+		
+		}
 	
+	
+	deletePersonHandler = (personIndex) => {
+		//const persons = this.state.persons.slice(); 
+		const persons = [...this.state.persons]; // Create a copy of your persons array 
+		persons.splice(personIndex, 1); // Remove one element from the array
+		this.setState({persons: persons}); // Update your persons array
+	}
+	
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+		// If doesShow is false, then it will set showPersons to true.
+		this.setState({showPersons: !doesShow}); 
+	}
+	
+  render () {
 	const style = {
 		backgroundColor: 'white',
 		font: 'inherit',
@@ -45,35 +69,41 @@ const app = props => {
 		padding: '8px',
 		cursor: 'pointer'
 	};
+	 
+	let persons = null;
+	  
+	if (this.state.showPersons) {
+		persons = (			
+		// Map will take persons array [line 7] and render it to the screen
+		<div>
+			{this.state.persons.map((person, index) => {
+			  // Return the person component
+			  return <Person name={person.name} 
+					  age={person.age}
+					  click={() => this.deletePersonHandler(index)} 
+	  				  key={person.id} 
+	  				  changed={(event) => this.nameChangedHandler(event, person.id)}/>
+			})}
+		</div>
+	  
+		);
+	}
 	
 	 return (
 	  // Cannot use class but rather className. Which is a react attribute
-      <div className="App">
+     <div className="App">
 		<h1>Hi, I am a react app!</h1>
 		<p>This is really working :D.</p>
 		 <button 
 		 style={style}	 
-		 onClick={() => switchNameHandler('Quang Phong!')}>Switch Name</button>
-		
-		<Person 
-		name={personsState.persons[0].name} 
-		age={personsState.persons[0].age} />
-		
-		<Person 
-		name={personsState.persons[1].name} 
-		age={personsState.persons[1].age} 
-		click={switchNameHandler.bind(this, 'Mary')} 
-		// nameChangeHandler used here
-		changed={nameChangedHandler}>My Hobbies: Racing</Person>
-		
-		<Person 
-		name={personsState.persons[2].name} 
-		age={personsState.persons[2].age} />
-      </div>
+		 onClick={this.togglePersonsHandler}>Switch Name</button>
+		{persons}
+	</div>
 
     );
 	 // createElement method - Takes in at least 3 args (Component, props, ...children)
 	 // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
-export default app;
+export default App;
